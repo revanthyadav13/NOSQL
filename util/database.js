7f3639b2bcf975ddac7f7309839ac34d320219e0
revanthyadav13 <revanthyadav13@gmail.com>
2023-10-05 14:36:21 +0530

    Getting Orders Placed

--- util/database.js
+++ util/database.js
@@ -5,21 +5,12 @@ let _db;
 
 const mongoConnect = callback => {
   MongoClient.connect(
-<<<<<<< HEAD
     'mongodb+srv://revanthyadav13:Admission%4010@cluster0.ybysmcy.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp'
   )
     .then(client => {
       console.log('Connected!');
       _db = client.db();
       callback();
-=======
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
-  )
-    .then(client => {
-      console.log('Connected!');
-      _db=client.db();
-      callback(client);
->>>>>>> origin/main
     })
     .catch(err => {
       console.log(err);
@@ -27,7 +18,6 @@ const mongoConnect = callback => {
     });
 };
 
-<<<<<<< HEAD
 const getDb = () => {
   if (_db) {
     return _db;
@@ -37,14 +27,3 @@ const getDb = () => {
 
 exports.mongoConnect = mongoConnect;
 exports.getDb = getDb;
-=======
-const getDb=()=>{
-  if(_db){
-    return _db;
-  }
-  throw 'No database Found!'
-}
-
-exports.mongoConnect = mongoConnect;
-exports.getDb=getDb;
->>>>>>> origin/main
