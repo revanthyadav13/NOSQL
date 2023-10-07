4ad2281aa9e3a81190208d5e7b6525fa7742ee8e
revanthyadav13 <revanthyadav13@gmail.com>
2023-10-07 14:48:41 +0530

    User Table

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -18,14 +18,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('5baa2528563f16379fc8a610')
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById('5bab316ce0a7c75f783cb8a8')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -34,9 +34,22 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    'mongodb+srv://revanth:admission@cluster0.ybysmcy.mongodb.net/shop?retryWrites=true&w=majority&appName=AtlasApp',{ useNewUrlParser: true }
+    'mongodb+srv://revanth:fEChn28xpRPQZf5m@cluster0.ybysmcy.mongodb.net/shop?retryWrites=true&w=majority&appName=AtlasApp',
+    { useNewUrlParser: true }
   )
   .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Max',
+          email: 'max@test.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
     app.listen(3000);
   })
   .catch(err => {
